503bb2d57499afe44cf6ef4c13efe18fc1bd6640
iingwen <129480634+iingwen@users.noreply.github.com>
2021-02-17 18:44:49 +0000

    Java 8 compatibility

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -5,3 +5,12 @@ pub mod retry;
 
 use chrono::{DateTime, Utc};
 use http::Method;
+use log::debug;
+use once_cell::sync::Lazy;
+use reqwest::{
+    blocking::{multipart::Form, Client as HttpClient, Response as HttpResponse},
+    header::{self, HeaderMap, HeaderValue},
+    IntoUrl, Proxy, Result as ReqwestResult,
+};
+use resources::{
+    bucket_statistics::GetBucketStat
