dd42ec869fb9e63669703a210f215609ce4902a5
iingwen <129480634+iingwen@users.noreply.github.com>
2022-07-04 20:48:50 +0000

    splice: sendfile() at once fails for big files

--- api/src/resources/audit.rs
+++ api/src/resources/audit.rs
@@ -85,3 +85,118 @@ struct AuditUser {
     tenant_id: AuditTenantId,
     username: Username,
 }
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct AuditQueryResponse {
+    audit_events: Vec<AuditEvent>,
+    projects: Vec<AuditProject>,
+    pub continuation: Option<Continuation>,
+    datasets: Vec<AuditDataset>,
+    tenants: Vec<AuditTenant>,
+    users: Vec<AuditUser>,
+}
+
+impl AuditQueryResponse {
+    pub fn into_iter_printable(self) -> QueryResponseIterator {
+        QueryResponseIterator {
+            response: self,
+            index: 0,
+        }
+    }
+
+    fn get_user(&self, user_id: &UserId) -> Option<&AuditUser> {
+        self.users.iter().find(|user| user.id == *user_id)
+    }
+
+    fn get_dataset(&self, dataset_id: &DatasetId) -> Option<&AuditDataset> {
+        self.datasets
+            .iter()
+            .find(|dataset| dataset.id == *dataset_id)
+    }
+
+    fn get_project(&self, project_id: &ProjectId) -> Option<&AuditProject> {
+        self.projects
+            .iter()
+            .find(|project| project.id == *project_id)
+    }
+
+    fn get_tenant(&self, tenant_id: &AuditTenantId) -> Option<&AuditTenant> {
+        self.tenants.iter().find(|tenant| tenant.id == *tenant_id)
+    }
+}
+
+pub struct QueryResponseIterator {
+    response: AuditQueryResponse,
+    index: usize,
+}
+
+impl Iterator for QueryResponseIterator {
+    type Item = PrintableAuditEvent;
+    fn next(&mut self) -> Option<Self::Item> {
+        let event = self.response.audit_events.get(self.index)?;
+
+        let actor_email = &self
+            .response
+            .get_user(&event.actor_user_id)
+            .unwrap_or_else(|| panic!("Could not find user for id `{}`", event.actor_user_id.0))
+            .email;
+
+        let dataset_names = event
+            .dataset_ids
+            .iter()
+            .map(|dataset_id| {
+                &self
+                    .response
+                    .get_dataset(dataset_id)
+                    .unwrap_or_else(|| panic!("Could not get dataset for id `{}`", dataset_id.0))
+                    .name
+            })
+            .cloned()
+            .collect();
+
+        let project_names = event
+            .project_ids
+            .iter()
+            .map(|project_id| {
+                &self
+                    .response
+                    .get_project(project_id)
+                    .unwrap_or_else(|| panic!("Could not get project for id `{}`", project_id.0))
+                    .name
+            })
+            .cloned()
+            .collect();
+
+        let tenant_names = event
+            .tenant_ids
+            .iter()
+            .map(|tenant_id| {
+                &self
+                    .response
+                    .get_tenant(tenant_id)
+                    .unwrap_or_else(|| panic!("Could not get tenant for id `{}`", tenant_id.0))
+                    .name
+            })
+            .cloned()
+            .collect();
+
+        let actor_tenant_name = &self
+            .response
+            .get_tenant(&event.actor_tenant_id)
+            .unwrap_or_else(|| panic!("Could not get tenant for id `{}`", event.actor_tenant_id.0))
+            .name;
+
+        self.index += 1;
+
+        Some(PrintableAuditEvent {
+            event_type: event.event_type.clone(),
+            actor_tenant_name: actor_tenant_name.clone(),
+            event_id: event.event_id.clone(),
+            timestamp: event.timestamp,
+            actor_email: actor_email.clone(),
+            dataset_names,
+            project_names,
+            tenant_names,
+        })
+    }
+}
---
+++ api/src/resources/bucket.rs
@@ -0,0 +1,5 @@
+use chrono::{DateTime, Utc};
+use once_cell::sync::Lazy;
+use regex::Regex;
+use serde::{Deserialize, Serialize};
+use std::fmt::{Display, Formatter, Result as 
