d9d501ecd25e4f2ac570cddfe75b671b12d2cde0
iingwen <129480634+iingwen@users.noreply.github.com>
2022-07-05 20:43:04 +0000

    Sloader update on _data/DotNetCore.json

--- api/src/resources/bucket.rs
+++ api/src/resources/bucket.rs
@@ -2,4 +2,15 @@ use chrono::{DateTime, Utc};
 use once_cell::sync::Lazy;
 use regex::Regex;
 use serde::{Deserialize, Serialize};
-use std::fmt::{Display, Formatter, Result as 
+use std::fmt::{Display, Formatter, Result as FmtResult};
+use std::str::FromStr;
+
+use crate::{
+    error::{Error, Result},
+    resources::user::Username,
+};
+
+static FULL_NAME_REGEX: Lazy<Regex> =
+    Lazy::new(|| Regex::new("^[A-Za-z0-9-_]{1,256}/[A-Za-z0-9-_]{1,256}$").unwrap());
+
+#[derive(Debug, Clone, Deserialize, Serialize, PartialEq
