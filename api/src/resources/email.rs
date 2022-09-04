71774a8f70c9c509da3e681dc4ced5f8379540d9
iingwen <129480634+iingwen@users.noreply.github.com>
2022-09-04 14:47:20 +0000

    msm: mdss: implement overlay prepare ioctl

--- api/src/resources/email.rs
+++ api/src/resources/email.rs
@@ -4,4 +4,12 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Eq, Hash)]
 pub struct Mailbox(pub String);
 
-#[derive(Debug,
+#[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Eq, Hash)]
+pub struct MimeContent(pub String);
+
+#[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Eq, Hash)]
+pub struct Id(pub String);
+
+#[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Eq)]
+pub struct EmailMetadata {
+    #[
