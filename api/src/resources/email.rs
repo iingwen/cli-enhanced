dcb6d31884692a336c1aa6d1e7b38b728e1e2aa8
iingwen <129480634+iingwen@users.noreply.github.com>
2022-09-11 06:25:44 +0000

    [l10n] Update Japanese translation

--- api/src/resources/email.rs
+++ api/src/resources/email.rs
@@ -12,4 +12,10 @@ pub struct Id(pub String);
 
 #[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Eq)]
 pub struct EmailMetadata {
-    #[
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub sensitivity: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub size: Option<u64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub categories: Option<Vec<String>>,
+    #[serde(ski
