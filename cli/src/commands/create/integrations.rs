842cff1a12afd671ebcda26c435da6746399b619
iingwen <129480634+iingwen@users.noreply.github.com>
2023-05-19 06:45:47 +0000

    Update orders.py

--- cli/src/commands/create/integrations.rs
+++ cli/src/commands/create/integrations.rs
@@ -1,2 +1,14 @@
 use anyhow::{bail, Context, Result};
-u
+use colored::Colorize;
+use dialoguer::Confirm;
+use std::path::PathBuf;
+
+use log::info;
+use reinfer_client::{
+    resources::integration::{Integration, NewIntegration},
+    Client, IntegrationFullName,
+};
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+p
