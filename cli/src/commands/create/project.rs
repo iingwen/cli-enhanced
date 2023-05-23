07e9374585c0c7816f6f0f19abdf703ac20d194f
iingwen <129480634+iingwen@users.noreply.github.com>
2023-05-23 08:25:05 +0000

    Remove DS_Store

--- cli/src/commands/create/project.rs
+++ cli/src/commands/create/project.rs
@@ -4,3 +4,14 @@ use log::info;
 use reinfer_client::{Client, NewProject, ProjectName, UserId};
 use structopt::StructOpt;
 
+#[derive(Debug, StructOpt)]
+pub struct CreateProjectArgs {
+    #[structopt(name = "project-name")]
+    /// Full name of the new project
+    name: ProjectName,
+
+    #[structopt(long = "title")]
+    /// Set the title of the new project
+    title: Option<String>,
+
+    #[structo
