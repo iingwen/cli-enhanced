f47eb98657d5a1e7c968303a2310ef1ee3863744
iingwen <129480634+iingwen@users.noreply.github.com>
2023-05-25 11:33:41 +0000

    BK automatic LOD removal.

--- cli/src/commands/create/stream_exception.rs
+++ cli/src/commands/create/stream_exception.rs
@@ -1,3 +1,12 @@
 use crate::printer::Printer;
 use anyhow::{Context, Result};
-use log::in
+use log::info;
+use reinfer_client::{
+    Client, CommentUid, StreamException, StreamExceptionMetadata, StreamFullName,
+};
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+pub struct CreateStreamExceptionArgs {
+    #[structopt(long = "stream")]
+    /// The stream full name, qualified by da
