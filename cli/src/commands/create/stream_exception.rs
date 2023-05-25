711b3fe25acc260b7bc7151ddc7b2b1f65ac7db5
iingwen <129480634+iingwen@users.noreply.github.com>
2023-05-25 18:59:50 +0000

    drm/radeon/evergreen+: wait for the MC to settle after MC blackout

--- cli/src/commands/create/stream_exception.rs
+++ cli/src/commands/create/stream_exception.rs
@@ -9,4 +9,12 @@ use structopt::StructOpt;
 #[derive(Debug, StructOpt)]
 pub struct CreateStreamExceptionArgs {
     #[structopt(long = "stream")]
-    /// The stream full name, qualified by da
+    /// The stream full name, qualified by dataset, such as 'my-project-name/my-dataset-name/my-stream-name'.
+    stream: StreamFullName,
+
+    #[structopt(long = "type")]
+    /// The type of exception. Please choose a short, easy-to-understand string such as "No Prediction".
+    r#type: String,
+
+    #[structopt(long = "uid")]
+    /
