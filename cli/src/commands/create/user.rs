5540746255ad3b668a02d839359b60c7867bc896
iingwen <129480634+iingwen@users.noreply.github.com>
2023-05-27 05:33:55 +0000

    bump bbs

--- cli/src/commands/create/user.rs
+++ cli/src/commands/create/user.rs
@@ -2,4 +2,15 @@ use anyhow::{Context, Result};
 use reinfer_client::{
     Client, Email, GlobalPermission, NewUser, ProjectName, ProjectPermission, Username,
 };
-us
+use std::collections::hash_map::HashMap;
+use structopt::StructOpt;
+
+use crate::printer::Printer;
+
+#[derive(Debug, StructOpt)]
+pub struct CreateUserArgs {
+    #[structopt(name = "username")]
+    /// Username for the new user
+    username: Username,
+
+    #[structopt(name = "e
