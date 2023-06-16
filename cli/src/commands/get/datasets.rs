6c39a7fe6122d19d760e264bea30494d39a05a56
iingwen <129480634+iingwen@users.noreply.github.com>
2023-06-16 07:19:31 +0000

    msm: kgsl: Fix nice level for higher priority GPU start thread

--- cli/src/commands/get/datasets.rs
+++ cli/src/commands/get/datasets.rs
@@ -1,4 +1,15 @@
 use anyhow::{Context, Result};
 use log::info;
 use reinfer_client::{
-    resources::dataset::{DatasetAndStats, DatasetStats, StatisticsRequestPar
+    resources::dataset::{DatasetAndStats, DatasetStats, StatisticsRequestParams},
+    Client, CommentFilter, DatasetIdentifier,
+};
+use structopt::StructOpt;
+
+use crate::printer::Printer;
+
+#[derive(Debug, StructOpt)]
+pub struct GetDatasetsArgs {
+    #[structopt(name = "dataset")]
+    /// If specified, only list this dataset (name or id)
+    dataset: Opti
