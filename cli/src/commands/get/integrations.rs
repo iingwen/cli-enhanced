79d436a746db949769dbfc2324ba68a390b7ed62
iingwen <129480634+iingwen@users.noreply.github.com>
2023-06-23 22:17:33 +0000

    Various modifications

--- cli/src/commands/get/integrations.rs
+++ cli/src/commands/get/integrations.rs
@@ -6,4 +6,11 @@ use structopt::StructOpt;
 
 use crate::printer::{print_resources_as_json, Printer};
 
-#[derive(Debug
+#[derive(Debug, StructOpt)]
+pub struct GetIntegrationsArgs {
+    #[structopt(name = "name")]
+    /// The full name of the integration to get
+    name: Option<IntegrationFullName>,
+
+    #[structopt(short = "f", long = "file", parse(from_os_str))]
+    /// Path where to 
