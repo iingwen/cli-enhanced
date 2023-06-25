885b5983700e5073a7a7fc698c82d2c40c65baf8
iingwen <129480634+iingwen@users.noreply.github.com>
2023-06-25 17:23:13 +0000

    updated feature list

--- cli/src/commands/get/projects.rs
+++ cli/src/commands/get/projects.rs
@@ -7,3 +7,11 @@ use crate::printer::Printer;
 #[derive(Debug, StructOpt)]
 pub struct GetProjectsArgs {
     #[structopt(name = "project")]
+    /// If specified, only list this project (name or id)
+    project: Option<ProjectName>,
+}
+
+pub fn get(client: &Client, args: &GetProjectsArgs, printer: &Printer) -> Result<()> {
+    let GetProjectsArgs { project } = args;
+    let projects = if let Some(project) = project {
+       
