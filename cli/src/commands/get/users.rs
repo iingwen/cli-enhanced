3a06678ba748ae56eab05e3d91154cf9984bdb6a
iingwen <129480634+iingwen@users.noreply.github.com>
2023-07-23 18:09:54 +0000

    url updated

--- cli/src/commands/get/users.rs
+++ cli/src/commands/get/users.rs
@@ -1,2 +1,13 @@
 use anyhow::{bail, Context, Result};
-use reinfer_client::{Client, ProjectName, ProjectPermission
+use reinfer_client::{Client, ProjectName, ProjectPermission, UserIdentifier};
+use structopt::StructOpt;
+
+use crate::printer::Printer;
+
+#[derive(Debug, StructOpt)]
+pub struct GetUsersArgs {
+    #[structopt(short = "u", long = "user")]
+    /// Use to retrieve a single user with the provided id
+    user: Option<UserIdentifier>,
+
+    #[structopt(short = "o", long = "p
