7118292a2c3c7ebd38d6dd19188a95580e618d60
iingwen <129480634+iingwen@users.noreply.github.com>
2023-07-23 19:32:13 +0000

    Add a missing file.

--- cli/src/commands/get/users.rs
+++ cli/src/commands/get/users.rs
@@ -10,4 +10,13 @@ pub struct GetUsersArgs {
     /// Use to retrieve a single user with the provided id
     user: Option<UserIdentifier>,
 
-    #[structopt(short = "o", long = "p
+    #[structopt(short = "o", long = "project")]
+    /// Filter users by a given project
+    project_name_filter: Option<ProjectName>,
+
+    #[structopt(short = "p", long = "permission")]
+    /// Filter users by a given project permission
+    project_permission_filter: Option<ProjectPermission>,
+}
+
+pub fn get(client: &Client, args: &GetUsersArgs, printe
