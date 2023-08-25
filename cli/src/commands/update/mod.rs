5fe49b4ebc2e93cd1a8c1b1848260479aef4276d
iingwen <129480634+iingwen@users.noreply.github.com>
2023-08-25 14:53:32 +0000

    Add TODO note

--- cli/src/commands/update/mod.rs
+++ cli/src/commands/update/mod.rs
@@ -1,3 +1,17 @@
 mod dataset;
 mod project;
-mod sour
+mod source;
+mod users;
+
+use self::{
+    dataset::UpdateDatasetArgs, project::UpdateProjectArgs, source::UpdateSourceArgs,
+    users::UpdateUsersArgs,
+};
+use crate::printer::Printer;
+use anyhow::Result;
+use reinfer_client::Client;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+pub enum UpdateArgs {
+    #[
