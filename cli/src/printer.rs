2dc0cfbcda2216afaec54930652baf09cedafe2a
iingwen <129480634+iingwen@users.noreply.github.com>
2023-09-02 16:46:45 +0000

    update tmux

--- cli/src/printer.rs
+++ cli/src/printer.rs
@@ -142,3 +142,283 @@ impl DisplayTable for DatasetAndStats {
             self.dataset.owner.0.dimmed(),
             "/".dimmed(),
             self.dataset.name.0
+        );
+        row![
+            full_name,
+            self.dataset.id.0,
+            self.dataset.updated_at.format("%Y-%m-%d %H:%M:%S"),
+            self.dataset.title,
+            self.stats.total_verbatims,
+            self.stats.num_reviewed
+        ]
+    }
+}
+
+impl DisplayTable for Project {
+    fn to_table_headers() -> Row {
+        row![bFg => "Name", "ID", "Title"]
+    }
+
+    fn to_table_row(&self) -> Row {
+        row![
+            self.name.0,
+            match &self.id {
+                Some(id) => id.0.as_str().into(),
+                None => "unknown".dimmed(),
+            },
+            self.title
+        ]
+    }
+}
+
+impl DisplayTable for Source {
+    fn to_table_headers() -> Row {
+        row![bFg => "Name", "ID", "Updated (UTC)", "Transform Tag", "Title"]
+    }
+
+    fn to_table_row(&self) -> Row {
+        let full_name = format!("{}{}{}", self.owner.0.dimmed(), "/".dimmed(), self.name.0);
+        row![
+            full_name,
+            self.id.0,
+            self.updated_at.format("%Y-%m-%d %H:%M:%S"),
+            match &self.transform_tag {
+                Some(transform_tag) => transform_tag.0.as_str().into(),
+                None => "missing".dimmed(),
+            },
+            self.title,
+        ]
+    }
+}
+
+#[derive(Debug)]
+pub struct PrintableBucket {
+    pub bucket: Bucket,
+    pub stats: Option<BucketStatistics>,
+}
+impl DisplayTable for PrintableBucket {
+    fn to_table_headers() -> Row {
+        row![bFg => "Name", "ID", "Created (UTC)", "Num Emails"]
+    }
+
+    fn to_table_row(&self) -> Row {
+        let full_name = format!(
+            "{}{}{}",
+            self.bucket.owner.0.dimmed(),
+            "/".dimmed(),
+            self.bucket.name.0
+        );
+        row![
+            full_name,
+            self.bucket.id.0,
+            self.bucket.created_at.format("%Y-%m-%d %H:%M:%S"),
+            if let Some(stats) = &self.stats {
+                stats.count.to_string().as_str().into()
+            } else {
+                "none".dimmed()
+            }
+        ]
+    }
+}
+impl Serialize for PrintableBucket {
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        Serialize::serialize(&self.bucket, serializer)
+    }
+}
+
+/// Source with additional fields for printing
+/// Serializes to a Source
+#[derive(Debug)]
+pub struct PrintableSource {
+    pub source: Source,
+    pub bucket: Option<Bucket>,
+    pub stats: Option<CommentStatistics>,
+}
+
+impl Serialize for PrintableSource {
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        Serialize::serialize(&self.source, serializer)
+    }
+}
+
+impl DisplayTable for PrintableSource {
+    fn to_table_headers() -> Row {
+        row![bFg => "Name", "ID", "Updated (UTC)", "Transform Tag", "Bucket", "Title", "Num Comments"]
+    }
+
+    fn to_table_row(&self) -> Row {
+        let full_name = format!(
+            "{}{}{}",
+            self.source.owner.0.dimmed(),
+            "/".dimmed(),
+            self.source.name.0
+        );
+        row![
+            full_name,
+            self.source.id.0,
+            self.source.updated_at.format("%Y-%m-%d %H:%M:%S"),
+            match &self.source.transform_tag {
+                Some(transform_tag) => transform_tag.0.as_str().into(),
+                None => "missing".dimmed(),
+            },
+            match &self.bucket {
+                Some(bucket) => bucket.name.0.as_str().into(),
+                None => match &self.source.bucket_id {
+                    Some(bucket_id) => bucket_id.0.as_str().dimmed(),
+                    None => "none".dimmed(),
+                },
+            },
+            self.source.title,
+            if let Some(stats) = &self.stats {
+                stats.num_comments.to_string().as_str().into()
+            } else {
+                "none".dimmed()
+            }
+        ]
+    }
+}
+
+impl DisplayTable for Stream {
+    fn to_table_headers() -> Row {
+        row![bFg => "Name", "ID", "Updated (UTC)", "Title"]
+    }
+
+    fn to_table_row(&self) -> Row {
+        row![
+            self.name.0,
+            self.id.0,
+            self.updated_at.format("%Y-%m-%d %H:%M:%S"),
+            self.title
+        ]
+    }
+}
+
+impl DisplayTable for User {
+    fn to_table_headers() -> Row {
+        row![bFg => "Name", "Email", "ID", "Created (UTC)", "Global Permissions"]
+    }
+
+    fn to_table_row(&self) -> Row {
+        row![
+            self.username.0,
+            self.email.0,
+            self.id.0,
+            self.created_at.format("%Y-%m-%d %H:%M:%S"),
+            self.global_permissions
+                .iter()
+                .chain(self.sso_global_permissions.iter())
+                .map(|permission| permission.to_string())
+                .collect::<Vec<String>>()
+                .join(", ")
+        ]
+    }
+}
+
+impl DisplayTable for PrintableAuditEvent {
+    fn to_table_headers() -> Row {
+        row![bFg => "Timestamp", "Event Id", "Event Type", "Actor Email", "Actor Tenant", "Dataset Names",  "Project Names", "Tenant Names"]
+    }
+
+    fn to_table_row(&self) -> Row {
+        row![
+            self.timestamp,
+            self.event_id.0,
+            self.event_type.0,
+            self.actor_email.0,
+            self.actor_tenant_name.0,
+            if self.dataset_names.is_empty() {
+                "none".dimmed()
+            } else {
+                self.dataset_names
+                    .iter()
+                    .map(|dataset| dataset.0.clone())
+                    .collect::<Vec<String>>()
+                    .join(" & ")
+                    .normal()
+            },
+            if self.project_names.is_empty() {
+                "none".dimmed()
+            } else {
+                self.project_names
+                    .iter()
+                    .map(|project| project.0.clone())
+                    .collect::<Vec<String>>()
+                    .join(" & ")
+                    .normal()
+            },
+            if self.tenant_names.is_empty() {
+                "none".dimmed()
+            } else {
+                self.tenant_names
+                    .iter()
+                    .map(|name| name.0.clone())
+                    .collect::<Vec<String>>()
+                    .join(" & ")
+                    .normal()
+            }
+        ]
+    }
+}
+
+/// Helper trait to allow collection of resources to be converted into a table.
+pub trait IntoTable {
+    fn into_table(self) -> Table;
+}
+
+/// All iterators of resources can be converted into a table.
+impl<'a, Iterable, Item: 'a> IntoTable for Iterable
+where
+    Iterable: IntoIterator<Item = &'a Item>,
+    Item: DisplayTable,
+{
+    fn into_table(self) -> Table {
+        let mut table = new_table();
+        table.set_titles(Item::to_table_headers());
+        for source in self.into_iter() {
+            table.add_row(source.to_table_row());
+        }
+        table
+    }
+}
+
+fn new_table() -> Table {
+    let mut table = Table::new();
+    let format = format::FormatBuilder::new()
+        .column_separator(' ')
+        .borders(' ')
+        .separators(&[], format::LineSeparator::new('-', '+', '+', '+'))
+        .padding(0, 1)
+        .build();
+    table.set_format(format);
+    table
+}
+
+fn print_table<T: IntoTable>(resources: T) {
+    let table = resources.into_table();
+    table.printstd();
+}
+
+/// Print resources using the selected output format.
+///
+/// Resources passed to the printer must be able to be formatted using all supported
+/// `OutputFormat`s.
+#[derive(Default, Debug)]
+pub struct Printer {
+    output: OutputFormat,
+}
+
+impl Printer {
+    pub fn new(output: OutputFormat) -> Self {
+        Self { output }
+    }
+
+    pub fn print_resources<T, Resource>(&self, resources: T) -> Result<()>
+    where
+        T: IntoIterator<Item = Resource> + IntoTable,
+        Resource: Serialize,
