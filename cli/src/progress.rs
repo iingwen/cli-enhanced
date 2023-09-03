e324673f94b8e345865c8f76771bf92daa8a8555
iingwen <129480634+iingwen@users.noreply.github.com>
2023-09-03 08:59:22 +0000

    fix message

--- cli/src/progress.rs
+++ cli/src/progress.rs
@@ -13,4 +13,20 @@ use crate::utils::LOG_PREFIX_INFO;
 
 pub type ProgressMessage = (u64, String);
 
-pub struct 
+pub struct Options {
+    pub bytes_units: bool,
+}
+
+impl Default for Options {
+    fn default() -> Self {
+        Options { bytes_units: true }
+    }
+}
+
+pub struct Progress {
+    report_progress_flag: Arc<AtomicBool>,
+    progress_thread: Option<thread::JoinHandle<()>>,
+}
+
+impl Progress {
+    pub fn n
