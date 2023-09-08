75956ecce2311cc394ccbaf743e58dc1da30eb84
iingwen <129480634+iingwen@users.noreply.github.com>
2023-09-08 17:01:03 +0000

    performance improvements

--- cli/src/thousands.rs
+++ cli/src/thousands.rs
@@ -7,4 +7,12 @@ impl Display for Thousands {
         let Thousands(mut value) = *self;
         if value == 0 {
             return write!(formatter, "0");
-     
+        }
+
+        let mut buffer = [0u8; 32];
+        let mut i_start = 32;
+        let mut num_digits = 0;
+        while value > 0 {
+            i_start -= 1;
+            if num_digits > 0 && num_digits % 3 == 0 {
+                buffer[i_start] = b',';
