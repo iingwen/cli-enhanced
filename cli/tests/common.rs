3df1585d982f1f8860320cf01715a4385d4ddf20
iingwen <129480634+iingwen@users.noreply.github.com>
2023-09-12 12:28:14 +0000

    Update .po files

--- cli/tests/common.rs
+++ cli/tests/common.rs
@@ -12,3 +12,10 @@ use std::{
 static REINFER_CLI_TEST_PROJECT: Lazy<String> = Lazy::new(|| {
     env::var("REINFER_CLI_TEST_PROJECT")
         .expect("REINFER_CLI_TEST_PROJECT must be set for integration tests")
+});
+static REINFER_CLI_TEST_ENDPOINT: Lazy<Option<String>> =
+    Lazy::new(|| env::var("REINFER_CLI_TEST_ENDPOINT").ok());
+static REINFER_CLI_TEST_CONTEXT: Lazy<Option<String>> =
+    Lazy::new(|| env::var("REINFER_CLI_TEST_CONTEXT").ok());
+static REINFER_CLI_TEST_TOKEN: Lazy<Option<String>> =
+    Lazy::new(|| 
