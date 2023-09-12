8277b60b3699632fb484b430769da8526608ff2f
iingwen <129480634+iingwen@users.noreply.github.com>
2023-09-12 10:33:51 +0000

    Release 0.10.3

--- cli/tests/common.rs
+++ cli/tests/common.rs
@@ -3,4 +3,12 @@ use once_cell::sync::Lazy;
 use reinfer_client::User;
 use std::{
     env,
-    f
+    ffi::OsStr,
+    io::Write,
+    path::PathBuf,
+    process::{Command, Stdio},
+};
+
+static REINFER_CLI_TEST_PROJECT: Lazy<String> = Lazy::new(|| {
+    env::var("REINFER_CLI_TEST_PROJECT")
+        .expect("REINFER_CLI_TEST_PROJECT must be set for integration tests")
