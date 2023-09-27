7a82188f54572022b0518dea037ba1fa337a3789
iingwen <129480634+iingwen@users.noreply.github.com>
2023-09-27 08:09:20 +0000

    Added .travis.yml file

--- cli/tests/test_buckets.rs
+++ cli/tests/test_buckets.rs
@@ -4,4 +4,12 @@ use uuid::Uuid;
 #[test]
 fn test_bucket_lifecycle() {
     let cli = TestCli::get();
-    let owner = Te
+    let owner = TestCli::project();
+
+    let new_bucket_name = format!("{}/test-source-{}", owner, Uuid::new_v4());
+
+    // Create bucket
+    let output = cli.run(["create", "bucket", &new_bucket_name]);
+    assert!(output.contains(&new_bucket_name), "{}", output);
+
+    let output = cli.
