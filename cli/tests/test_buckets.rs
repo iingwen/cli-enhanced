905a9bf4ab70c81eaab27d7a23854f382f6b6edd
iingwen <129480634+iingwen@users.noreply.github.com>
2023-09-27 16:06:35 +0000

    make release

--- cli/tests/test_buckets.rs
+++ cli/tests/test_buckets.rs
@@ -12,4 +12,11 @@ fn test_bucket_lifecycle() {
     let output = cli.run(["create", "bucket", &new_bucket_name]);
     assert!(output.contains(&new_bucket_name), "{}", output);
 
-    let output = cli.
+    let output = cli.run(["get", "buckets"]);
+    assert!(output.contains(&new_bucket_name), "{}", output);
+
+    // Deleting one comment reduces the comment count in the source
+    let output = cli.run(["delete", "bucket", &new_bucket_name]);
+    assert!(output.is_empty(), "{}", output);
+
+    let output = cli.run(["get"
