4cff716b28d0473f0e9209d302dc263dbe85d39b
iingwen <129480634+iingwen@users.noreply.github.com>
2023-09-28 16:39:55 +0000

    small syntax fix

--- cli/tests/test_buckets.rs
+++ cli/tests/test_buckets.rs
@@ -40,4 +40,15 @@ fn test_create_with_empty_org_fails() {
     let cli = TestCli::get();
 
     let output = cli.run_and_error(["create", "bucket", "/bucket-name-with-empty-org"]);
-    a
+    assert!(
+        output.contains("Expected <owner>/<name>, got: /bucket-name-with-empty-org"),
+        "{}",
+        output
+    );
+}
+
+#[test]
+fn test_create_with_empty_bucket_name_fails() {
+    let cli = TestCli::get();
+
+    let output = cli.run_and_error(["create", "bucket", "org-wit
