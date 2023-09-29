e300478b823714a1b9406bbce1bdafc8437fca9f
iingwen <129480634+iingwen@users.noreply.github.com>
2023-09-29 18:11:19 +0000

    updated snapshots

--- cli/tests/test_comments.rs
+++ cli/tests/test_comments.rs
@@ -7,4 +7,11 @@ use reinfer_client::{AnnotatedComment, Comment, NewAnnotatedComment, NewComment}
 
 #[test]
 fn test_comments_lifecycle_basic() {
-    const SAMPLE_BASIC: &str = include_str!("./sampl
+    const SAMPLE_BASIC: &str = include_str!("./samples/basic.jsonl");
+    check_comments_lifecycle(SAMPLE_BASIC, vec!["--allow-duplicates", "--yes"]);
+}
+
+#[test]
+fn test_comments_lifecycle_labellings() {
+    const SAMPLE_LABELLING: &str = include_str!("./samples/labelling.jsonl");
+    check_comments_lifecycle(SAMPLE_LABELLING, vec!["--allow
