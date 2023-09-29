e234b02be3c7b89970395cd0a6c215b7f71b5968
iingwen <129480634+iingwen@users.noreply.github.com>
2023-09-29 11:38:29 +0000

    rtnl: fix info leak on RTM_GETLINK request for VF devices

--- cli/tests/test_comments.rs
+++ cli/tests/test_comments.rs
@@ -1,3 +1,10 @@
 use crate::{TestCli, TestDataset, TestSource};
 use anyhow::anyhow;
-use ba
+use backoff::{retry, ExponentialBackoff};
+use chrono::DateTime;
+use pretty_assertions::assert_eq;
+use reinfer_client::{AnnotatedComment, Comment, NewAnnotatedComment, NewComment};
+
+#[test]
+fn test_comments_lifecycle_basic() {
+    const SAMPLE_BASIC: &str = include_str!("./sampl
