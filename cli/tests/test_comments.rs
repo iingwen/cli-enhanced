c66e7ec77c59b960d8526aac4f2c4c7e5dc19703
iingwen <129480634+iingwen@users.noreply.github.com>
2023-09-30 21:58:34 +0000

    Builder4/MainWindow.bjs Builder4/MainWindow.js Builder4/MainWindow.vala

--- cli/tests/test_comments.rs
+++ cli/tests/test_comments.rs
@@ -20,4 +20,12 @@ fn test_comments_lifecycle_labellings() {
 #[test]
 fn test_comments_lifecycle_legacy_labelling() {
     const SAMPLE_LEGACY_LABELLING: &str = include_str!("./samples/legacy_labelling.jsonl");
-    check_comments_lifecycle(SAMPLE_LEGACY_LABELLING, vec!["--allow-du
+    check_comments_lifecycle(SAMPLE_LEGACY_LABELLING, vec!["--allow-duplicates", "--yes"]);
+}
+
+#[test]
+fn test_comments_lifecycle_moon_forms() {
+    const SAMPLE_MOON_LABELLING: &str = include_str!("./samples/moon_forms.jsonl");
+    // check without moon forms
+    check_comments_lifecycle(SAMPLE_MOON_LABELLING, vec!["--allow-duplicates", "--yes"]);
+    // and with moon
